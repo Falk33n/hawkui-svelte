77aed02eb9d81553295acf477475950d0a17e694
Falk33n <tim.falk00@gmail.com>
2025-03-30 13:32:04 +0200

    feat: initialize accordion

---
+++ src/lib/components/ui/accordion/index.ts
@@ -0,0 +1,18 @@
+import Content from './accordion-content.svelte';
+import Header from './accordion-header.svelte';
+import Item from './accordion-item.svelte';
+import Trigger from './accordion-trigger.svelte';
+import Root from './accordion.svelte';
+
+export {
+	Root as Accordion,
+	Content as AccordionContent,
+	Header as AccordionHeader,
+	Item as AccordionItem,
+	Trigger as AccordionTrigger,
+	Content,
+	Header,
+	Item,
+	Root,
+	Trigger
+};
