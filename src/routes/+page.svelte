2e2ec415ea81c82f6142794027a4ff0b89f62647
Falk33n <tim.falk00@gmail.com>
2025-04-01 10:54:13 +0200

    feat: begin on avatar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,33 @@
 <script lang="ts">
-	import * as Accordion from '$lib/components/ui/accordion';
+	import * as Accordion from '../lib/components/ui/accordion';
 </script>
 
-<div class="mx-auto my-10 flex w-[80%] flex-col items-center justify-center space-y-10">
+<div class="flex flex-col justify-center items-center space-y-10 mx-auto my-10 w-[80%]">
 	<Accordion.Root type="single">
 		<Accordion.Item>
 			<Accordion.Trigger>Trigger1</Accordion.Trigger>
 			<Accordion.Content>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, laboriosam
-				perspiciatis ut fuga, ab reiciendis nesciunt saepe vitae reprehenderit sint quia,
-				labore voluptatibus ullam expedita ducimus. Ipsum nemo atque nam?
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptates
+				veniam similique magnam rerum saepe officiis cumque labore atque, dolores velit
+				obcaecati libero. Officiis beatae eius natus, voluptatibus magni soluta?
 			</Accordion.Content>
 		</Accordion.Item>
 
 		<Accordion.Item>
 			<Accordion.Trigger>Trigger2</Accordion.Trigger>
 			<Accordion.Content>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, laboriosam
-				perspiciatis ut fuga, ab reiciendis nesciunt saepe vitae reprehenderit sint quia,
-				labore voluptatibus ullam expedita ducimus. Ipsum nemo atque nam?
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptates
+				veniam similique magnam rerum saepe officiis cumque labore atque, dolores velit
+				obcaecati libero. Officiis beatae eius natus, voluptatibus magni soluta?
 			</Accordion.Content>
 		</Accordion.Item>
 
 		<Accordion.Item>
 			<Accordion.Trigger>Trigger3</Accordion.Trigger>
 			<Accordion.Content>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto, laboriosam
-				perspiciatis ut fuga, ab reiciendis nesciunt saepe vitae reprehenderit sint quia,
-				labore voluptatibus ullam expedita ducimus. Ipsum nemo atque nam?
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur voluptates
+				veniam similique magnam rerum saepe officiis cumque labore atque, dolores velit
+				obcaecati libero. Officiis beatae eius natus, voluptatibus magni soluta?
 			</Accordion.Content>
 		</Accordion.Item>
 	</Accordion.Root>
